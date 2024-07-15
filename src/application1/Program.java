4a5e01f963f400c7d462c43ba6889b1d641c1eef
viniciusandrre <viniciusandre_pereira@outlook.com>
2024-07-15 13:21:47 -0300

    Criando as classes das entidades, seus atriubtos e construtores

---
+++ src/application1/Program.java
@@ -0,0 +1,10 @@
+package application1;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/model/entities1/AluguelDeCarro.java
@@ -0,0 +1,5 @@
+package model.entities1;
+
+public class AluguelDeCarro {
+
+}
